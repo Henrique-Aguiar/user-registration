2b74a38807d6d0c83ddb5f08e1391cbfbc0c9de0
Henrique-Aguiar <hrikelr@hotmail.com>
2022-05-15 22:49:52 -0300

    criando o footer

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => 
+  <footer className="footer">
+    <span>
+      Desenvolvido com <i className='fa fa-heart text-danger'></i> por <strong className='text-danger'> Henrique</strong>
+    </span>
+  </footer>
