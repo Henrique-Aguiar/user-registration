8ba1f00df683a50fd35262cc613648cf5688351d
Henrique-Aguiar <hrikelr@hotmail.com>
2022-05-16 20:09:58 -0300

    implementando rotas

--- frontend/src/components/main/App.jsx
+++ frontend/src/components/main/App.jsx
@@ -2,17 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
+import Routes from './Routes'
 import Logo from '../template/Logo'
 import Nav from '../template/Nav'
-import Main from '../template/Main'
 import Footer from '../template/Footer'
 
 export default props => 
-  <div className="app">
-    <Logo />
-    <Nav />
-    <Main icon="home" title="Início"
-      subtitle="Segundo Projeto do capítulo de React."/>
-    <Footer />
-  </div>
+  <BrowserRouter>
+    <div className="app">
+      <Logo />
+      <Nav />
+      <Routes />
+      <Footer />
+    </div>
+  </BrowserRouter>
---
+++ frontend/src/components/user/UserCrud.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Main from "../template/Main";
+
+const headerProps = {
+  icon: 'users',
+  title: 'Usuário',
+  subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!'
+}
+
+export default class UserCrud extends Component {
+  render() {
+    return (
+      <Main {...headerProps}>
+        Cadastro de usuário
+      </Main>
+    )
+  }
+}
